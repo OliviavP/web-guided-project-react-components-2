7ab04b6f91b1d32dfb776cbb77cd56449902f5dc
OliviavP <livalossy@gmail.com>
2022-04-19 11:08:57 -0600

    unfinished

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -2,18 +2,23 @@ import React from 'react'
 import PetsList from './PetsList'
 
 export default function Friend(props) {
+  console.log(props.myFriend)
   // 👉 1- What does a Friend need?
 
   return (
     <div className='friend-friends container'>
       {/* 👉 2- Fix the JSX so it displays real info coming into the component */}
       <div className='friend-info'>
-        <div >
-          <h3>Name: Jessica</h3>
-          <p>Age: 26</p>
+        <div>
+          <h3>Name: {props.myFriend.name}</h3>
+          <p>Age: {props.myFriend.age}</p>
 
-          <p>Married: yes <button>change</button></p>
-          <div>Likes:
+          <p>
+            Married: {props.myFriend.married}
+            <button>change</button>
+          </p>
+          <div>
+            Likes:
             <ul>
               {/* 👉 3- Loop over the friend's hobbies and generate <li/> elements as you go */}
             </ul>
